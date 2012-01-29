7f859a13e5ed73607589c5f92ae4e7073aa0da86
Ryan Doom <ryan.doom@webascender.com>
2012-01-29 18:24:44 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SEOManagerAA::Application.config.secret_token = '1d12521009b8d7d56adc321c88bc5b7865af5c4944cc4875c685cc8dc41a6847008cfbb68a4c0c8f0bdd46c0c379344ada07281e85dd8b83f4e06ff2f051a69a'
